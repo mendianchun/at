49020cae260a5366d36637826aff3d1bf208a37f
Men Dianchun <mendianchun@acttao.com>
2015-12-30 11:43:17 +0800

    rollback

--- Daemon/Daemon.php
+++ Daemon/Daemon.php
@@ -4,7 +4,7 @@
  * *************************************************
  * Created on :2015-12-25 12:07:12
  * Encoding   :UTF-8
- * Description:1111
+ * Description:
  *
  * @Author 大门 <mendianchun@acttao.com>
  * ************************************************
