cd61196f56a7a59fc747b18371539caa83550a0a
Men Dianchun <mendianchun@acttao.com>
2016-01-11 11:38:28 +0800

    支持https

--- Daemon/Class/Http.class.php
+++ Daemon/Class/Http.class.php
@@ -4,20 +4,22 @@
  * $Id: class.Http.php
  * Http操作
  * Author::2012-09-10 daly
-//[2] 发送GET请求
-$http->get('http://www.sina.com.cn/');
-
-//[3] 取得返回的头数据
-print_r($http->getHeaders());
-
-//[4] 取得返回的数据
-echo $http->getContent();
+ * //[2] 发送GET请求
+ * $http->get('http://www.sina.com.cn/');
+ *
+ * //[3] 取得返回的头数据
+ * print_r($http->getHeaders());
+ *
+ * //[4] 取得返回的数据
+ * echo $http->getContent();
  */
 
 define('HTTP_METHOD_GET', 'GET');
 define('HTTP_METHOD_POST', 'POST');
 
-class Http {
+class Http
+{
+    var $scheme;
     var $host;
     var $port;
     var $path;
@@ -50,7 +52,8 @@ class Http {
     var $redirect_count = 0;
     var $cookie_host = '';
 
-    function __construct() {
+    function __construct()
+    {
 
     }
 
@@ -59,7 +62,8 @@ class Http {
      * 设置超时时间
      * @param $t 秒
      */
-    function setTimeout($t) {
+    function setTimeout($t)
+    {
         $this->timeout = $t;
     }
 
@@ -68,7 +72,9 @@ class Http {
      * @param $host 地址
      * @param $port 端口
      */
-    function setAddr($host, $port = 80) {
+    function setAddr($scheme, $host, $port = 80)
+    {
+        $this->scheme = $scheme;
         $this->host = $host;
         $this->port = $port;
     }
@@ -80,11 +86,12 @@ class Http {
      * @param mix $data 需要传递的数据 可以是 string 或者 array
      * @return bool 0:不成功, 1:成功
      */
-    function doGet($uri, $data = false) {
+    function doGet($uri, $data = false)
+    {
         $this->path = $uri;
         $this->method = HTTP_METHOD_GET;
         if ($data) {
-            $this->path .= '?'.$this->buildQueryString($data);
+            $this->path .= '?' . $this->buildQueryString($data);
         }
         return $this->doRequest();
     }
@@ -96,24 +103,26 @@ class Http {
      * @param mix $data 需要传递的数据 可以是 string 或者 array
      * @return bool 0:不成功, 1:成功
      */
-    function doPost($uri, $data) {
+    function doPost($uri, $data)
+    {
         $this->path = $uri;
         $this->method = HTTP_METHOD_POST;
         $this->postdata = $this->buildQueryString($data);
         return $this->doRequest();
     }
 
-    function buildQueryString($data) {
+    function buildQueryString($data)
+    {
         $querystring = '';
         if (is_array($data)) {
             // Change data in to postable data
             foreach ($data as $key => $val) {
                 if (is_array($val)) {
                     foreach ($val as $val2) {
-                        $querystring .= urlencode($key).'='.urlencode($val2).'&';
+                        $querystring .= urlencode($key) . '=' . urlencode($val2) . '&';
                     }
                 } else {
-                    $querystring .= urlencode($key).'='.urlencode($val).'&';
+                    $querystring .= urlencode($key) . '=' . urlencode($val) . '&';
                 }
             }
             $querystring = substr($querystring, 0, -1); // Eliminate unnecessary &
@@ -127,10 +136,12 @@ class Http {
      * 发起请求
      * @return 返回是否请求成功
      */
-    function doRequest() {
-
-        if (!$fp = @fsockopen($this->host, $this->port, $errno, $errstr, $this->timeout)) {
-            switch($errno) {
+    function doRequest()
+    {
+        $protocol = 'https' == $this->scheme ? 'ssl://' : '';
+//        var_dump($this->host,$this->port);
+        if (!$fp = @fsockopen($protocol . $this->host, $this->port, $errno, $errstr, $this->timeout)) {
+            switch ($errno) {
                 case -3:
                     $this->errormsg = 'Socket creation failed (-3)';
                 case -4:
@@ -138,8 +149,8 @@ class Http {
                 case -5:
                     $this->errormsg = 'Connection refused or timed out (-5)';
                 default:
-                    $this->errormsg = 'Connection failed ('.$errno.')';
-                    $this->errormsg .= ' '.$errstr;
+                    $this->errormsg = 'Connection failed (' . $errno . ')';
+                    $this->errormsg .= ' ' . $errstr;
                     $this->debug($this->errormsg);
             }
             //echo $errno;
@@ -166,20 +177,22 @@ class Http {
         $data = '';
 
 
-        if(!$status['timed_out']) {
+        if (!$status['timed_out']) {
             $data = @fgets($fp);
 
             if (!preg_match('/HTTP\/(\d\.\d)\s+(\d+)\s+(.*)/', $data, $m)) {
                 return false;
             }
 
-            $http_version = $m[1]; $this->status = $m[2]; $status_string = $m[3];
+            $http_version = $m[1];
+            $this->status = $m[2];
+            $status_string = $m[3];
 
 
             while (!feof($fp)) {
-                if($data = fgets($fp)) {
+                if ($data = fgets($fp)) {
 
-                    if($data == "\r\n" || $data == "\n") {
+                    if ($data == "\r\n" || $data == "\n") {
                         break;
                     } else {
                         if (!preg_match('/([^:]+):\\s*(.*)/', $data, $m)) {
@@ -202,7 +215,7 @@ class Http {
 
             if (!$this->headers_only) {
                 $data = '';
-                while(!feof($fp)) {
+                while (!feof($fp)) {
                     $data = fread($fp, 8192);
                     $this->content .= $data;
                 }
@@ -235,9 +248,9 @@ class Http {
                 foreach ($p as $cval) {
                     list($ckey, $crow) = explode('=', $cval);
                     $ckey = ltrim($ckey);
-                    if($ckey == 'path') {
+                    if ($ckey == 'path') {
                         $this->cookies[$cname][$ckey] = $crow;
-                    } elseif($ckey == 'expires') {
+                    } elseif ($ckey == 'expires') {
                         $this->cookies[$cname][$ckey] = strtotime($crow);
                     }
                 }
@@ -247,23 +260,27 @@ class Http {
         }
         // If $persist_referers, set the referer ready for the next request
         if ($this->persist_referers) {
-            $this->debug('Persisting referer: '.$this->getRequestURL());
+            $this->debug('Persisting referer: ' . $this->getRequestURL());
             $this->referer = $this->getRequestURL();
         }
         // Finally, if handle_redirects and a redirect is sent, do that
         if ($this->handle_redirects) {
             if (++$this->redirect_count >= $this->max_redirects) {
-                $this->errormsg = 'Number of redirects exceeded maximum ('.$this->max_redirects.')';
+                $this->errormsg = 'Number of redirects exceeded maximum (' . $this->max_redirects . ')';
                 $this->debug($this->errormsg);
                 $this->redirect_count = 0;
                 return false;
             }
             $location = isset($this->headers['location']) ? $this->headers['location'] : '';
             $uri = isset($this->headers['uri']) ? $this->headers['uri'] : '';
-            if($location || $uri) {
-                $url = parse_url($location.$uri);
-                if(!empty($url['host'])) {
-                    $this->setAddr($url['host']);
+            if ($location || $uri) {
+                $url = parse_url($location . $uri);
+                if (!empty($url['host'])) {
+                    $scheme = $url['scheme'];
+                    $host = $url['host'];
+                    $port = isset($url['port']) ? $url['port'] : ('https' == $scheme ? 443 : 80);
+                    $url['path'] = isset($url['path']) ? $url['path'] : '/';
+                    $this->setAddr($scheme, $host, $port);
                 }
                 return $this->doGet($url['path'], $url['query']);
             }
@@ -271,7 +288,8 @@ class Http {
         return true;
     }
 
-    function buildRequest() {
+    function buildRequest()
+    {
         $headers = array();
         $headers[] = "$this->method $this->path HTTP/1.0";
         $headers[] = "Host: {$this->host}";
@@ -294,30 +312,34 @@ class Http {
         }
         // Basic authentication
         if ($this->username && $this->password) {
-            $headers[] = 'Authorization: BASIC '.base64_encode($this->username.':'.$this->password);
+            $headers[] = 'Authorization: BASIC ' . base64_encode($this->username . ':' . $this->password);
         }
         // If this is a POST, set the content type and length
         if ($this->postdata) {
             $headers[] = 'Content-Type: application/x-www-form-urlencoded';
-            $headers[] = 'Content-Length: '.strlen($this->postdata);
+            $headers[] = 'Content-Length: ' . strlen($this->postdata);
         }
-        $request = implode("\r\n", $headers)."\r\n\r\n".$this->postdata;
+        $request = implode("\r\n", $headers) . "\r\n\r\n" . $this->postdata;
         return $request;
     }
 
-    function getStatus() {
+    function getStatus()
+    {
         return $this->status;
     }
 
-    function getContent() {
+    function getContent()
+    {
         return $this->content;
     }
 
-    function getHeaders() {
+    function getHeaders()
+    {
         return $this->headers;
     }
 
-    function getHeader($header) {
+    function getHeader($header)
+    {
         $header = strtolower($header);
         if (isset($this->headers[$header])) {
             return $this->headers[$header];
@@ -326,63 +348,76 @@ class Http {
         }
     }
 
-    function getError() {
+    function getError()
+    {
         return $this->errormsg;
     }
 
-    function getCookies() {
+    function getCookies()
+    {
         return $this->cookies;
     }
 
-    function getRequestURL() {
-        $url = 'http://'.$this->host;
+    function getRequestURL()
+    {
+        $url = 'http://' . $this->host;
         if ($this->port != 80) {
-            $url .= ':'.$this->port;
+            $url .= ':' . $this->port;
         }
         $url .= $this->path;
         return $url;
     }
 
     // Setter methods
-    function setUserAgent($string) {
+    function setUserAgent($string)
+    {
         $this->user_agent = $string;
     }
 
-    function setAuthorization($username, $password) {
+    function setAuthorization($username, $password)
+    {
         $this->username = $username;
         $this->password = $password;
     }
 
-    function setCookies($array) {
+    function setCookies($array)
+    {
         $this->cookies = $array;
     }
 
     // Option setting methods
-    function useGzip($boolean) {
+    function useGzip($boolean)
+    {
         $this->use_gzip = $boolean;
     }
 
-    function setPersistCookies($boolean) {
+    function setPersistCookies($boolean)
+    {
         $this->persist_cookies = $boolean;
     }
 
-    function setPersistReferers($boolean) {
+    function setPersistReferers($boolean)
+    {
         $this->persist_referers = $boolean;
     }
 
-    function setHandleRedirects($boolean) {
+    function setHandleRedirects($boolean)
+    {
         $this->handle_redirects = $boolean;
     }
 
-    function setMaxRedirects($num) {
+    function setMaxRedirects($num)
+    {
         $this->max_redirects = $num;
     }
 
-    function setHeadersOnly($boolean) {
+    function setHeadersOnly($boolean)
+    {
         $this->headers_only = $boolean;
     }
 
-    function setDebug($boolean) {
+    function setDebug($boolean)
+    {
         $this->debug = $boolean;
     }
 
@@ -392,15 +427,17 @@ class Http {
      * @param string $url URL地址 需要带上http://开头
      * @return bool 0:不成功, 1:成功
      */
-    function get($url,$data=false) {
+    function get($url, $data = false)
+    {
         $bits = parse_url($url);
+        $scheme = $bits['scheme'];
         $host = $bits['host'];
-        $port = isset($bits['port']) ? $bits['port'] : 80;
+        $port = isset($bits['port']) ? $bits['port'] : ('https' == $scheme ? 443 : 80);
         $path = isset($bits['path']) ? $bits['path'] : '/';
         if (isset($bits['query'])) {
-            $path .= '?'.$bits['query'];
+            $path .= '?' . $bits['query'];
         }
-        $this->setAddr($host, $port);
+        $this->setAddr($scheme, $host, $port);
         return $this->doGet($path, $data);
     }
 
@@ -411,25 +448,28 @@ class Http {
      * @param mix $data 需要发送的数据 字符串或者数组
      * @return bool 0:不成功, 1:成功
      */
-    function post($url, $data) {
+    function post($url, $data)
+    {
         $bits = parse_url($url);
+        $scheme = $bits['scheme'];
         $host = $bits['host'];
-        $port = isset($bits['port']) ? $bits['port'] : 80;
+        $port = isset($bits['port']) ? $bits['port'] : ('https' == $scheme ? 443 : 80);
         $path = isset($bits['path']) ? $bits['path'] : '/';
-        $this->setAddr($host, $port);
+        $this->setAddr($scheme, $host, $port);
         return $this->doPost($path, $data);
     }
 
 
-    function debug($msg, $object = false) {
+    function debug($msg, $object = false)
+    {
         if ($this->debug) {
-            print '<div style="border: 1px solid red; padding: 0.5em; margin: 0.5em;"><strong>HttpClient Debug:</strong> '.$msg;
+            print '<div style="border: 1px solid red; padding: 0.5em; margin: 0.5em;"><strong>HttpClient Debug:</strong> ' . $msg;
             if ($object) {
                 ob_start();
                 print_r($object);
                 $content = htmlentities(ob_get_contents());
                 ob_end_clean();
-                print '<pre>'.$content.'</pre>';
+                print '<pre>' . $content . '</pre>';
             }
             print '</div>';
         }
